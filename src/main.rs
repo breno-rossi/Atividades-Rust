786767561a315ca63f176d6b7db4b3c4e4fcf4f3
Breno Rossi <brenorossiduarte@gmail.com>
2025-08-10 17:11:25 -0300

    parte 1 feita

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+
+    //1. Declaring Integer Variables
+
+    let a: i8 = 32;
+    let b: i16 = 54;
+    let c: i32 = 766;
+    let d: i64 = 8966;
+    let e: i128 = 9876656;
+
+    let f: u8 = 233;
+    let g: u16 = 1500;
+    let h: u32 =5123455;
+    let i: u64 = 64566545;
+    let j: u128 = 76786546;
+
+    println!("{}", {a},);
+    println!("{}", {b},);
+    println!("{}", {c},);
+    println!("{}", {d},);
+    println!("{}", {e},);
+    println!("{}", {f},);
+    println!("{}", {g},);
+    println!("{}", {h},);
+    println!("{}", {i},);
+    println!("{}", {j},);
+
+
+
 }
